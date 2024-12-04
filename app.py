965e51d8bfe6e43bc24aaaaf887612c75a89cfe9
AnTaewoo <atw13730@gmail.com>
2024-12-05 02:26:37 +0900

    * 기존 git commit 복구
    ADD: mvc pattern backend auth logic
    * add app.py, config,py : reference to templete.env and create .env file
    * add model.py : 실질적인 logic. 오직 controller.py를 에 대해 종속된다
    * add controller.py : model의 결과를 view.py에 전송
    * add view.py : app_route를 통해 method와 route를 수신받은 후, 결과를 return

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from config import create_database, create_connection, create_table
+from view.view import app_route
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+app.register_blueprint(app_route)
+
+if __name__ == "__main__":
+    create_database()  # 데이터베이스 생성 (없으면 생성)
+    connection = create_connection()
+    if connection:
+        create_table(connection)  # 테이블 생성 (없으면 생성)
+        connection.close()
+    app.run(debug=True)
